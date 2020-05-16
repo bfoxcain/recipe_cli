a64ea7aa93b4e29461ee7b25f48d541fb558fef3
bfoxcain <bfcain19@gmail.com>
2020-05-16 10:51:34 -0500

    update

--- lib/cookbook/cli.rb
+++ lib/cookbook/cli.rb
@@ -4,5 +4,8 @@ class Cli
         puts "  "
         puts " Hello and welcome to my Recipe search app."
         puts "  "
+        puts "Enter an ingredient to see recipes with that ingredient."
+        puts "  "
+        @ingredient = gets.strip.downcase
     end
 end
