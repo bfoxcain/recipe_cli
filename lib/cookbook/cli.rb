02b5b96ff0f9cf79b9fac656ff8eb52ac3960490
bfoxcain <bfcain19@gmail.com>
2020-05-16 10:47:10 -0500

    Done.

--- lib/cookbook/cli.rb
+++ lib/cookbook/cli.rb
@@ -1,5 +1,8 @@
 ## responsible for handling input and output to user
 class Cli
-
-
+    def run
+        puts "  "
+        puts " Hello and welcome to my Recipe search app."
+        puts "  "
+    end
 end
--- lib/environment.rb
+++ lib/environment.rb
@@ -2,3 +2,7 @@
 require "httparty"
 require "json"
 require "pry"
+
+require_relative "./cookbook/cli"
+require_relative "./cookbook/api"
+require_relative "./cookbook/recipe"
